37aa58b3047b117886b06abc147e697b1489248c
BRIWA <salaheddinezouitni00@gmail.com>
2024-04-05 00:01:36 +0000

    tailwind fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import { ChakraProvider } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App'
-import "./output.css";
+import "./index.css";
 
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
--- client/src/views/home.jsx
+++ client/src/views/home.jsx
@@ -3,7 +3,7 @@ import { Heading } from "@chakra-ui/react"
 const Home = ()=>{
     return(
         <>
-            <h1 className="uppercase text-red-800">hello world</h1>
+            <h1 className="uppercase text-red-800 m-10">hello world</h1>
             <Heading color='tomato'>I'm a Heading</Heading>
         </>
     )
